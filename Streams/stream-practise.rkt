6290e8d1f277293341687fb045512d42b790aa71
alexgeorgiew <77070782+alexgeorgiew@users.noreply.github.com>
2021-12-18 19:57:05 +0200

    Update stream-practise.rkt

--- Streams/stream-practise.rkt
+++ Streams/stream-practise.rkt
@@ -24,3 +24,36 @@
 (define (stream-map s f)
  (if (empty-stream? s) '() (cons (f (car s)) (delay (stream-map (tail s) f))))
   )
+
+(define (stream-filter s p)
+  (if (p (head s)) (cons (head s) (delay (stream-filter (tail s) p))) (stream-filter (tail s) p))
+        )
+
+(define (stream-fold s acc f)
+ (if (empty-stream? s) acc (stream-fold (tail s) (f acc (head s)) f))
+  )
+
+(define (stream->list s)   ;s has end
+  (if (empty-stream? s) '() (cons (head s) (stream->list (tail s))))
+  )
+
+(define (stream-drop s n)
+  (if (empty-stream? s) the-empty-stream (if (= n 0) s (stream-drop (tail s) (- n 1)))) 
+  )
+
+(define (stream-take s n)
+  (if (= n 0) the-empty-stream (cons (head s) (delay (stream-take (tail s) (- n 1)))))
+  )
+
+(define (natural-generator n)
+  (cons n (delay (natural-generator (+ n 1)))))
+
+(define naturals (natural-generator 0))
+
+(define (fibonacci-generator a b)
+  (cons (+ a b) (delay (fibonacci-generator b (+ a b))))
+  )
+
+(define fibonacci (cons 0 (delay (fibonacci-generator 0 1))))
+
+(define (stream-append s1 s2) (cons s1 (delay s2)))
